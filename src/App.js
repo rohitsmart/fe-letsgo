65eb93288a1dd882877b37ed1ab6c121afae2e69
rohitsmart <rohitsmart95@gmail.com>
2024-05-14 23:40:56 +0530

    dashboard issue resolved

--- src/App.js
+++ src/App.js
@@ -20,18 +20,19 @@ function App() {
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <Navbar />
-          <div style={{ marginTop: '64px' }}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/dashboard/*" element={<Dashboard />} />
-              <Route path="/dashboard/train/*" element={<Dashboard />} />
-              <Route path="/dashboard/flight/*" element={<Dashboard />} />
-              <Route path="/dashboard/hotel/*" element={<Dashboard />} />
-              <Route path="/dashboard/train/rail" element={<TrainManagement />} />
-              <Route path="/dashboard/flight/*" element={<FlightManagement />} />
-              <Route path="/dashboard/hotel/*" element={<HotelManagement />} />
-            </Routes>
+          <div style={{ marginTop: '64px', display: 'flex' }}>
+            {/* Always render Dashboard */}
+            <Dashboard />
+            <div style={{ marginLeft: '240px', flex: 1 }}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                {/* Add other routes here */}
+                <Route path="/dashboard/train/rail" element={<TrainManagement />} />
+                <Route path="/dashboard/flight/*" element={<FlightManagement />} />
+                <Route path="/dashboard/hotel/*" element={<HotelManagement />} />
+              </Routes>
+            </div>
           </div>
           <Footer />
         </BrowserRouter>
